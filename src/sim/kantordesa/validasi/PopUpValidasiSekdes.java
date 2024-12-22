1053637e8afe8dcbfe13c8df01ea8b01f22f7c8e
razikdontcare <gamersia4@gmail.com>
2024-12-22 16:58:27 +0800

    (refacotr): using built in FlatLightLaf.setup() to change the LookAndFeel

--- src/sim/kantordesa/validasi/PopUpValidasiSekdes.java
+++ src/sim/kantordesa/validasi/PopUpValidasiSekdes.java
@@ -5,6 +5,7 @@
 package sim.kantordesa.validasi;
 
 import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 import java.awt.FlowLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
@@ -777,29 +778,7 @@ public final class PopUpValidasiSekdes extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-            javax.swing.UIManager.setLookAndFeel(new FlatIntelliJLaf());
-        } catch (UnsupportedLookAndFeelException  ex) {
-            java.util.logging.Logger.getLogger(PopUpValidasiSekdes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex); }
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(sekdes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(sekdes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(sekdes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-        //</editor-fold>
+        FlatLightLaf.setup();
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
--- src/sim/kantordesa/validasi/ValidationPages.java
+++ src/sim/kantordesa/validasi/ValidationPages.java
@@ -5,6 +5,7 @@
 package sim.kantordesa.validasi;
 
 import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.UnsupportedLookAndFeelException;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -484,50 +485,7 @@ public class ValidationPages extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
-        // (optional) ">
-        /*
-         * If Nimbus (introduced in Java SE 6) is not available, stay with the default
-         * look and feel.
-         * For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            // for (javax.swing.UIManager.LookAndFeelInfo info :
-            // javax.swing.UIManager.getInstalledLookAndFeels()) {
-            // if ("Nimbus".equals(info.getName())) {
-            // javax.swing.UIManager.setLookAndFeel(info.getClassName());
-            // break;
-            // }
-            // }
-            javax.swing.UIManager.setLookAndFeel(new FlatIntelliJLaf());
-        } catch (UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ValidationPages.class.getName()).log(java.util.logging.Level.SEVERE,
-                    null, ex);
-        }
-        // } catch (InstantiationException ex) {
-        // java.util.logging.Logger.getLogger(sekdes.class.getName()).log(java.util.logging.Level.SEVERE,
-        // null, ex);
-        // } catch (IllegalAccessException ex) {
-        // java.util.logging.Logger.getLogger(sekdes.class.getName()).log(java.util.logging.Level.SEVERE,
-        // null, ex);
-        // } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-        // java.util.logging.Logger.getLogger(sekdes.class.getName()).log(java.util.logging.Level.SEVERE,
-        // null, ex);
-        // }
-        // </editor-fold>
-        // } catch (InstantiationException ex) {
-        // java.util.logging.Logger.getLogger(sekdes.class.getName()).log(java.util.logging.Level.SEVERE,
-        // null, ex);
-        // } catch (IllegalAccessException ex) {
-        // java.util.logging.Logger.getLogger(sekdes.class.getName()).log(java.util.logging.Level.SEVERE,
-        // null, ex);
-        // } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-        // java.util.logging.Logger.getLogger(sekdes.class.getName()).log(java.util.logging.Level.SEVERE,
-        // null, ex);
-        // }
-        // </editor-fold>
+        FlatLightLaf.setup();
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
